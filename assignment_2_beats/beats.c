952bda70d96fbb8e8f60be67b2e1aa94e5feab4c
sunyuan <s.lum@student.unsw.edu.au>
2023-09-04 07:13:55 +1000

    temp change

--- assignment_2_beats/beats.c
+++ assignment_2_beats/beats.c
@@ -65,8 +65,6 @@ struct note {
 
 // Add prototypes for any extra functions you create here.
 
-
-
 // Return a malloced Beat with fields initialized.
 Beat create_beat(void) {
     Beat new_beat = malloc(sizeof (struct beat));
